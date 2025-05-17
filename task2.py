14dc4f3f3967e5d5b8a6091141789a30b50b1339
yepcog <yep0cog41@gmail.com>
2025-05-18 00:49:04 +0300

    fixed function's behavior w incorrect arguments

--- task2.py
+++ task2.py
@@ -1,10 +1,18 @@
 import random
 
-def get_numbers_ticket(min=1, max=1000, quantity=6):
-    unique = []
-    unique = random.sample(range(min, max+1), quantity)
-    unique.sort()
-    return unique
+def get_numbers_ticket(min, max, quantity):
+    result = []
+    try:
+        random.sample(range(min, max+1), quantity)
+    except ValueError:
+        return result
+    else:
+        if min < 1 or max > 1000:
+            return result
+        else:
+            result = random.sample(range(min, max+1), quantity)
+            result.sort()
+            return result
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Winning lottery numbers:", lottery_numbers)
